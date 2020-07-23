d7ae3a9b1c90c91e91ef8bd33a2f61ce8fee7fa8
Yelena Strizhakova <e.strizhakova@gmail.com>
2020-07-23 09:58:34 +0300

    fix: btn

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,28 @@
 const initialCards = [
-    {
-        name: 'США',
-        link: './images/element-usa.JPG'
-    },
-    {
-        name: 'Бермудские острова',
-        link: './images/element-bermuda.JPG'
-    },
-    {
-        name: 'Германия',
-        link: './images/element-germany.JPG'
-    },
-    {
-        name: 'Италия',
-        link: './images/element-italy.JPG'
-    },
-    {
-        name: 'Португалия',
-        link: './images/element-portugal.JPG'
-    },
-    {
-        name: 'Французская Полинезия',
-        link: './images/element-french-polynesia.JPG'
-    }
+  {
+      name: 'США',
+      link: './images/element-usa.JPG'
+  },
+  {
+      name: 'Бермудские острова',
+      link: './images/element-bermuda.JPG'
+  },
+  {
+      name: 'Германия',
+      link: './images/element-germany.JPG'
+  },
+  {
+      name: 'Италия',
+      link: './images/element-italy.JPG'
+  },
+  {
+      name: 'Португалия',
+      link: './images/element-portugal.JPG'
+  },
+  {
+      name: 'Французская Полинезия',
+      link: './images/element-french-polynesia.JPG'
+  }
 ];
 
 const modal = document.querySelector('.modal');
@@ -53,41 +53,49 @@ toggleModal(modalNewCard));
 modalClosePicture.addEventListener('click', () =>
 toggleModal(modalPicture));
 
-
 const modalBtnProfile = modalProfile.querySelector('.modal__btn');
 const modalBtnNewCard = modalNewCard.querySelector('.modal__btn');
 
 function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-    
-    const nameInput = modalProfile.querySelector('.modal__name'); //Выбрала поле имени в модалке профиля
-    const jobInput = modalProfile.querySelector('.modal__occupation');  // Выбрала поле деятельности в модалке профиля
+  evt.preventDefault(); 
+  
+  const nameInput = modalProfile.querySelector('.modal__name'); //Выбрала поле имени в модалке профиля
+  const jobInput = modalProfile.querySelector('.modal__occupation');  // Выбрала поле деятельности в модалке профиля
 
-    const nameProfile = document.querySelector('.profile__name');
-    const jobProfile = document.querySelector('.profile__occupation'); // Выберите элементы, куда должны быть вставлены значения полей
-    
-    nameProfile.textContent =  nameInput.value;
-    jobProfile.textContent = jobInput.value;    // Вставьте новые значения с помощью textContent
+  const nameProfile = document.querySelector('.profile__name');
+  const jobProfile = document.querySelector('.profile__occupation'); // Выберите элементы, куда должны быть вставлены значения полей
+  
+  nameProfile.textContent =  nameInput.value;
+  jobProfile.textContent = jobInput.value;    // Вставьте новые значения с помощью textContent
 }
 
 const template = document.querySelector('.template').content;
 const elements__list = document.querySelector('.elements__list');
-    
 
 function cardSubmitHandler (evt) {
-    evt.preventDefault(); 
+  evt.preventDefault(); 
 
-    const cardNameInput = modalNewCard.querySelector('.modal__name'); //Выбрала поле имени в модалке карточки
-    const cardPicInput = modalNewCard.querySelector('.modal__occupation'); // Выбрала поле картинки в модалке карточки
+  const cardNameInput = modalNewCard.querySelector('.modal__name'); //Выбрала поле имени в модалке карточки
+  const cardPicInput = modalNewCard.querySelector('.modal__occupation'); // Выбрала поле картинки в модалке карточки
 
-    const card = template.cloneNode(true);//клонировала template;
+  const card = template.cloneNode(true);//клонировала template;
 
-    card.querySelector('.elements__image').src = cardPicInput.value;
-    card.querySelector('.elements__text').textContent = cardNameInput.value;
+  card.querySelector('.elements__image').src = cardPicInput.value;
+  card.querySelector('.elements__text').textContent = cardNameInput.value;
 
-    elements__list.prepend(card);
+  const cardLike = card.querySelector('.elements__like');
+  cardLike.addEventListener('click', function(evt) {
+      evt.target.classList.toggle('elements__like_active');
+  });
 
-    modalNewCard.querySelector('.modal__container').reset();
+  const cardRemove = card.querySelector('.elements__basket');
+  cardRemove.addEventListener('click', function(evt) {
+      evt.target.closest('.elements__item').remove();
+  });
+
+  elements__list.prepend(card);
+
+  modalNewCard.querySelector('.modal__container').reset();
 }
 
 // Прикрепляем обработчик к форме:
@@ -98,41 +106,41 @@ modalBtnProfile.addEventListener('click',  () =>
 toggleModal(modalProfile));
 modalBtnNewCard.addEventListener('click',  () =>
 toggleModal(modalNewCard));
- 
+
 function createCard(data) {
-    const card = template.cloneNode(true);
-    
-    card.querySelector('.elements__image').src = data.link;
-    card.querySelector('.elements__text').textContent = data.name;
-    
-    const cardLike = card.querySelector('.elements__like');
-    cardLike.addEventListener('click', function(evt) {
-        evt.target.classList.toggle('elements__like_active');
-});
+  const card = template.cloneNode(true);
+  
+  card.querySelector('.elements__image').src = data.link;
+  card.querySelector('.elements__text').textContent = data.name;
+  
+  const cardLike = card.querySelector('.elements__like');
+  cardLike.addEventListener('click', function(evt) {
+      evt.target.classList.toggle('elements__like_active');
+  });
 
-    const cardRemove = card.querySelector('.elements__basket');
-    cardRemove.addEventListener('click', function(evt) {
-        evt.target.closest('.elements__item').remove();
-});
+  const cardRemove = card.querySelector('.elements__basket');
+  cardRemove.addEventListener('click', function(evt) {
+      evt.target.closest('.elements__item').remove();
+  });
 
-    const cardImage = card.querySelector('.elements__image');
-    cardImage.addEventListener('click', () => {
+  const cardImage = card.querySelector('.elements__image');
+  cardImage.addEventListener('click', () => {
     const openImage = document.querySelector('.modal__picture-image'); 
     const pictureText =  document.querySelector('.modal__picture-text'); // Выберите элементы, куда должны быть вставлены значения
-    openImage.src = data.link;
-    pictureText.textContent = data.name;
-    /*openImage.src = card.querySelector('.elements__image').src; 
-    pictureText.textContent = card.querySelector('.elements__text').textContent;*/
+    /*openImage.src = data.link;
+    pictureText.textContent = data.name;*/
+    openImage.src = card.querySelector('.elements__image').src; 
+    pictureText.textContent = card.querySelector('.elements__text').textContent;
     toggleModal(modalPicture);
-   });
+  });
 
-   return card
+ return card
 }
 
 function renderCard(data) {
-    elements__list.prepend(createCard(data));
+  elements__list.prepend(createCard(data));
 }
 
 initialCards.forEach(function(data) {
-    renderCard(data);
-    });
+  renderCard(data);
+  });
