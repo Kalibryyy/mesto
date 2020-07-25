709123170946da3313041f977f0bf9eebd794534
Yelena Strizhakova <e.strizhakova@gmail.com>
2020-07-25 15:36:51 +0300

    fix: js

--- scripts/index.js
+++ scripts/index.js
@@ -56,49 +56,36 @@ toggleModal(modalPicture));
 const modalBtnProfile = modalProfile.querySelector('.modal__btn');
 const modalBtnNewCard = modalNewCard.querySelector('.modal__btn');
 
+const nameInput = modalProfile.querySelector('.modal__name'); //Выбрала поле имени в модалке профиля
+const jobInput = modalProfile.querySelector('.modal__occupation');  // Выбрала поле деятельности в модалке профиля
+
+const nameProfile = document.querySelector('.profile__name');
+const jobProfile = document.querySelector('.profile__occupation'); // Выбрала элементы, куда должны быть вставлены значения полей
+  
 function formSubmitHandler (evt) {
   evt.preventDefault(); 
   
-  const nameInput = modalProfile.querySelector('.modal__name'); //Выбрала поле имени в модалке профиля
-  const jobInput = modalProfile.querySelector('.modal__occupation');  // Выбрала поле деятельности в модалке профиля
-
-  const nameProfile = document.querySelector('.profile__name');
-  const jobProfile = document.querySelector('.profile__occupation'); // Выберите элементы, куда должны быть вставлены значения полей
-  
   nameProfile.textContent =  nameInput.value;
-  jobProfile.textContent = jobInput.value;    // Вставьте новые значения с помощью textContent
+  jobProfile.textContent = jobInput.value;    // Вставила новые значения с помощью textContent
 }
 
 const template = document.querySelector('.template').content;
-const elements__list = document.querySelector('.elements__list');
+const elementsList = document.querySelector('.elements__list');
+
+const cardNameInput = modalNewCard.querySelector('.modal__name'); //Выбрала поле имени в модалке карточки
+const cardPicInput = modalNewCard.querySelector('.modal__occupation'); // Выбрала поле картинки в модалке карточки
+
+const modalContainer = modalNewCard.querySelector('.modal__container');
 
 function cardSubmitHandler (evt) {
   evt.preventDefault(); 
 
-  const cardNameInput = modalNewCard.querySelector('.modal__name'); //Выбрала поле имени в модалке карточки
-  const cardPicInput = modalNewCard.querySelector('.modal__occupation'); // Выбрала поле картинки в модалке карточки
+  renderCard({name: cardNameInput.value, link: cardPicInput.value});
 
-  const card = template.cloneNode(true);//клонировала template;
-
-  card.querySelector('.elements__image').src = cardPicInput.value;
-  card.querySelector('.elements__text').textContent = cardNameInput.value;
-
-  const cardLike = card.querySelector('.elements__like');
-  cardLike.addEventListener('click', function(evt) {
-      evt.target.classList.toggle('elements__like_active');
-  });
-
-  const cardRemove = card.querySelector('.elements__basket');
-  cardRemove.addEventListener('click', function(evt) {
-      evt.target.closest('.elements__item').remove();
-  });
-
-  elements__list.prepend(card);
-
-  modalNewCard.querySelector('.modal__container').reset();
+  modalContainer.reset();
 }
 
-// Прикрепляем обработчик к форме:
+// Прикрепляю обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 modalProfile.addEventListener('submit', formSubmitHandler);
 modalNewCard.addEventListener('submit', cardSubmitHandler);
@@ -106,6 +93,9 @@ modalBtnProfile.addEventListener('click',  () =>
 toggleModal(modalProfile));
 modalBtnNewCard.addEventListener('click',  () =>
 toggleModal(modalNewCard));
+
+const openImage = document.querySelector('.modal__picture-image'); 
+const pictureText =  document.querySelector('.modal__picture-text'); // Выбрала элементы, куда должны быть вставлены значения
 
 function createCard(data) {
   const card = template.cloneNode(true);
@@ -125,20 +115,19 @@ function createCard(data) {
 
   const cardImage = card.querySelector('.elements__image');
   cardImage.addEventListener('click', () => {
-    const openImage = document.querySelector('.modal__picture-image'); 
-    const pictureText =  document.querySelector('.modal__picture-text'); // Выберите элементы, куда должны быть вставлены значения
     openImage.src = data.link;
+    openImage.alt = data.name;
     pictureText.textContent = data.name;
     /*openImage.src = card.querySelector('.elements__image').src; 
     pictureText.textContent = card.querySelector('.elements__text').textContent;*/
     toggleModal(modalPicture);
   });
 
- return card
+ return card;
 }
 
 function renderCard(data) {
-  elements__list.prepend(createCard(data));
+  elementsList.prepend(createCard(data));
 }
 
 initialCards.forEach(function(data) {
