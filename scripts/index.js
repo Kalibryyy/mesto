52e27c9fa787f721dd1fcbf3616a17061952be92
Yelena Strizhakova <e.strizhakova@gmail.com>
2020-08-07 21:21:07 +0300

    fix: UX

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,3 @@
-const initialCards = [{
-    name: 'США',
-    link: './images/element-usa.JPG'
-  },
-  {
-    name: 'Бермудские острова',
-    link: './images/element-bermuda.JPG'
-  },
-  {
-    name: 'Германия',
-    link: './images/element-germany.JPG'
-  },
-  {
-    name: 'Италия',
-    link: './images/element-italy.JPG'
-  },
-  {
-    name: 'Португалия',
-    link: './images/element-portugal.JPG'
-  },
-  {
-    name: 'Французская Полинезия',
-    link: './images/element-french-polynesia.JPG'
-  }
-];
-
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button');
 
@@ -35,87 +9,83 @@ const modalCloseProfile = modalProfile.querySelector('.modal__close');
 const modalCloseNewCard = modalNewCard.querySelector('.modal__close');
 const modalClosePicture = modalPicture.querySelector('.modal__close');
 
+const template = document.querySelector('.template').content;
+const elementsList = document.querySelector('.elements__list');
+
 function toggleModal(modal) {
-  modal.classList.toggle('modal_opened');
-  document.addEventListener('keydown', handleEscape);
+    modal.classList.toggle('modal_opened');
+    document.addEventListener('keydown', handleEscape);
 }
 
 function handleEscape(evt) {
-  if (evt.key === 'Escape') {
-    modalProfile.classList.remove('modal_opened');
-    modalNewCard.classList.remove('modal_opened');
-    modalPicture.classList.remove('modal_opened');
-  }
-
-  document.removeEventListener('keydown', handleEscape);
+    if (evt.key === 'Escape') {
+        modalProfile.classList.remove('modal_opened');
+        modalNewCard.classList.remove('modal_opened');
+        modalPicture.classList.remove('modal_opened');
+        document.removeEventListener('keydown', handleEscape);
+    }
 }
 
 editButton.addEventListener('click', () =>
-  toggleModal(modalProfile));
+    toggleModal(modalProfile));
 
 addButton.addEventListener('click', () =>
-  toggleModal(modalNewCard));
+    toggleModal(modalNewCard));
 
 modalCloseProfile.addEventListener('click', () =>
-  toggleModal(modalProfile));
+    toggleModal(modalProfile));
 
 modalCloseNewCard.addEventListener('click', () =>
-  toggleModal(modalNewCard));
+    toggleModal(modalNewCard));
 modalClosePicture.addEventListener('click', () =>
-  toggleModal(modalPicture));
+    toggleModal(modalPicture));
 
 const modalProfileOverlay = modalProfile.querySelector('.modal__overlay');
 const modalNewCardOverlay = modalNewCard.querySelector('.modal__overlay');
 const modalPictureOverlay = modalPicture.querySelector('.modal__picture-overlay');
 
-modalProfileOverlay.addEventListener('click', function () {
-  toggleModal(modalProfile);
+modalProfileOverlay.addEventListener('click', function() {
+    toggleModal(modalProfile);
 });
-modalNewCardOverlay.addEventListener('click', function () {
-  toggleModal(modalNewCard);
+modalNewCardOverlay.addEventListener('click', function() {
+    toggleModal(modalNewCard);
 });
-modalPictureOverlay.addEventListener('click', function () {
-  toggleModal(modalPicture);
+modalPictureOverlay.addEventListener('click', function() {
+    toggleModal(modalPicture);
 });
 
 function formSubmitHandler(evt) {
-  evt.preventDefault();
+    evt.preventDefault();
 
-  const nameInput = modalProfile.querySelector('.modal__input_type_name'); //Выбрала поле имени в модалке профиля
-  const jobInput = modalProfile.querySelector('.modal__input_type_occupation'); // Выбрала поле деятельности в модалке профиля
+    const nameInput = modalProfile.querySelector('.modal__input_type_name'); //Выбрала поле имени в модалке профиля
+    const jobInput = modalProfile.querySelector('.modal__input_type_occupation'); // Выбрала поле деятельности в модалке профиля
 
-  const nameProfile = document.querySelector('.profile__name');
-  const jobProfile = document.querySelector('.profile__occupation'); // Выберите элементы, куда должны быть вставлены значения полей
+    const nameProfile = document.querySelector('.profile__name');
+    const jobProfile = document.querySelector('.profile__occupation'); // Выберите элементы, куда должны быть вставлены значения полей
 
-  nameProfile.textContent = nameInput.value;
-  jobProfile.textContent = jobInput.value; // Вставьте новые значения с помощью textContent
+    nameProfile.textContent = nameInput.value;
+    jobProfile.textContent = jobInput.value; // Вставьте новые значения с помощью textContent
 
-  modalProfile.querySelector('.modal__container').reset();
-  modalBtnProfile.setAttribute('disabled', true);
-  modalBtnProfile.classList.add('modal__btn_disabled');
+    modalProfile.querySelector('.modal__container').reset();
 }
-
-const template = document.querySelector('.template').content;
-const elementsList = document.querySelector('.elements__list');
-//const card = template.cloneNode(true);//клонировала template;
 
 const modalBtnProfile = modalProfile.querySelector('.modal__btn');
 const modalBtnNewCard = modalNewCard.querySelector('.modal__btn');
 
 function cardSubmitHandler(evt) {
-  evt.preventDefault();
+    evt.preventDefault();
 
-  const cardNameInput = modalNewCard.querySelector('.modal__input_type_name'); // Выбрала поле имени в модалке карточки
-  const cardPicInput = modalNewCard.querySelector('.modal__input_type_occupation'); // Выбрала поле картинки в модалке карточки
+    const cardNameInput = modalNewCard.querySelector('.modal__input_type_name'); // Выбрала поле имени в модалке карточки
+    const cardPicInput = modalNewCard.querySelector('.modal__input_type_occupation'); // Выбрала поле картинки в модалке карточки
 
-  renderCard({
-    name: cardNameInput.value,
-    link: cardPicInput.value
-  });
+    renderCard({
+        name: cardNameInput.value,
+        link: cardPicInput.value
+    });
 
-  modalNewCard.querySelector('.modal__container').reset();
-  modalBtnNewCard.setAttribute('disabled', true);
-  modalBtnNewCard.classList.add('modal__btn_disabled');
+    modalNewCard.querySelector('.modal__container').reset();
+    modalBtnNewCard.disabled = true;
+    modalBtnNewCard.classList.add('modal__btn_disabled');
 }
 
 // Прикрепляем обработчик к форме:
@@ -124,45 +94,46 @@ modalProfile.addEventListener('submit', formSubmitHandler);
 modalNewCard.addEventListener('submit', cardSubmitHandler);
 
 modalBtnProfile.addEventListener('click', () =>
-  toggleModal(modalProfile));
+    toggleModal(modalProfile));
 modalBtnNewCard.addEventListener('click', () =>
-  toggleModal(modalNewCard));
+    toggleModal(modalNewCard));
 
 function createCard(data) {
-  const card = template.cloneNode(true);
+    const card = template.cloneNode(true);
+    const cardImage = card.querySelector('.elements__image');
 
-  card.querySelector('.elements__image').src = data.link;
-  card.querySelector('.elements__text').textContent = data.name;
+    cardImage.src = data.link;
+    card.querySelector('.elements__text').textContent = data.name;
 
-  const cardLike = card.querySelector('.elements__like');
-  cardLike.addEventListener('click', function (evt) {
-    evt.target.classList.toggle('elements__like_active');
-  });
+    const cardLike = card.querySelector('.elements__like');
+    cardLike.addEventListener('click', function(evt) {
+        evt.target.classList.toggle('elements__like_active');
+    });
 
-  const cardRemove = card.querySelector('.elements__basket');
-  cardRemove.addEventListener('click', function (evt) {
-    evt.target.closest('.elements__item').remove();
-  });
+    const cardRemove = card.querySelector('.elements__basket');
+    cardRemove.addEventListener('click', function(evt) {
+        evt.target.closest('.elements__item').remove();
+    });
 
-  const cardImage = card.querySelector('.elements__image');
-  cardImage.addEventListener('click', () => {
-    const openImage = document.querySelector('.modal__picture-image');
-    const pictureText = document.querySelector('.modal__picture-text'); // Выберите элементы, куда должны быть вставлены значения
-    openImage.src = data.link;
-    openImage.alt = data.name;
-    pictureText.textContent = data.name;
-    //openImage.src = card.querySelector('.elements__image').src; 
-    //pictureText.textContent = card.querySelector('.elements__text').textContent;
-    toggleModal(modalPicture);
-  });
 
-  return card
+    cardImage.addEventListener('click', () => {
+        const openImage = document.querySelector('.modal__picture-image');
+        const pictureText = document.querySelector('.modal__picture-text'); // Выберите элементы, куда должны быть вставлены значения
+        openImage.src = data.link;
+        openImage.alt = data.name;
+        pictureText.textContent = data.name;
+        //openImage.src = card.querySelector('.elements__image').src; 
+        //pictureText.textContent = card.querySelector('.elements__text').textContent;
+        toggleModal(modalPicture);
+    });
+
+    return card
 }
 
 function renderCard(data) {
-  elementsList.prepend(createCard(data));
+    elementsList.prepend(createCard(data));
 }
 
-initialCards.forEach(function (data) {
-  renderCard(data);
+initialCards.forEach(function(data) {
+    renderCard(data);
 });
--- scripts/validate.js
+++ scripts/validate.js
@@ -8,29 +8,29 @@ const object = {
 }
 
 // Функция, которая добавляет класс с ошибкой
-const showInputError = (formElement, inputElement, errorMessage, obj) => {
+const showInputError = (formElement, inputElement, errorMessage, params) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  inputElement.classList.add(obj.inputErrorClass);
+  inputElement.classList.add(params.inputErrorClass);
   errorElement.textContent = errorMessage;
-  errorElement.classList.add(obj.errorClass);
+  errorElement.classList.add(params.errorClass);
 };
 
 // Функция, которая удаляет класс с ошибкой
-const hideInputError = (formElement, inputElement, obj) => {
+const hideInputError = (formElement, inputElement, params) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  inputElement.classList.remove(obj.inputErrorClass);
-  errorElement.classList.remove(obj.errorClass);
+  inputElement.classList.remove(params.inputErrorClass);
+  errorElement.classList.remove(params.errorClass);
   errorElement.textContent = '';
 };
 
 // Функция, которая проверяет валидность поля
-const isValid = (formElement, inputElement, obj) => {
+const isValid = (formElement, inputElement, params) => {
   if (!inputElement.validity.valid) {
-    // Если поле не проходит валидацию, покажем ошибку
-    showInputError(formElement, inputElement, inputElement.validationMessage, obj);
+      // Если поле не проходит валидацию, покажем ошибку
+      showInputError(formElement, inputElement, inputElement.validationMessage, params);
   } else {
-    // Если проходит, скроем
-    hideInputError(formElement, inputElement, obj);
+      // Если проходит, скроем
+      hideInputError(formElement, inputElement, params);
   }
 };
 
@@ -38,65 +38,65 @@ const isValid = (formElement, inputElement, obj) => {
 const hasInvalidInput = (inputList) => {
   // проходим по этому массиву методом some
   return inputList.some((inputElement) => {
-    // Если поле не валидно, колбэк вернёт true
-    // Обход массива прекратится и вся фунцкция
-    // hasInvalidInput вернёт true
+      // Если поле не валидно, колбэк вернёт true
+      // Обход массива прекратится и вся фунцкция
+      // hasInvalidInput вернёт true
 
-    return !inputElement.validity.valid;
+      return !inputElement.validity.valid;
   })
 };
 
 // Функция принимает массив полей ввода
 // и элемент кнопки, состояние которой нужно менять
-const toggleButtonState = (inputList, buttonElement, obj) => {
+const toggleButtonState = (inputList, buttonElement, params) => {
   // Если есть хотя бы один невалидный инпут
   if (hasInvalidInput(inputList)) {
-    // сделай кнопку неактивной
-    buttonElement.classList.add(obj.inactiveButtonClass);
+      // сделай кнопку неактивной
+      buttonElement.classList.add(params.inactiveButtonClass);
+      buttonElement.disabled = true;
   } else {
-    // иначе сделай кнопку активной
-    buttonElement.classList.remove(obj.inactiveButtonClass);
-    buttonElement.removeAttribute("disabled");
+      // иначе сделай кнопку активной
+      buttonElement.classList.remove(params.inactiveButtonClass);
+      buttonElement.disabled = false;
   }
 };
 
-const setEventListeners = (formElement, obj) => {
+const setEventListeners = (formElement, params) => {
   // Находим все поля внутри формы,
   // сделаем из них массив методом Array.from
-  const inputList = Array.from(formElement.querySelectorAll(obj.inputSelector));
-  const buttonElement = formElement.querySelector(obj.submitButtonSelector);
+  const inputList = Array.from(formElement.querySelectorAll(params.inputSelector));
+  const buttonElement = formElement.querySelector(params.submitButtonSelector);
 
-  toggleButtonState(inputList, buttonElement, obj);
+  toggleButtonState(inputList, buttonElement, params);
 
   // Обойдём все элементы полученной коллекции
   inputList.forEach((inputElement) => {
-    // каждому полю добавим обработчик события input
-    inputElement.addEventListener('input', () => {
-      // Внутри колбэка вызовем isValid,
-      // передав ей форму и проверяемый элемент
-      isValid(formElement, inputElement, obj);
-      toggleButtonState(inputList, buttonElement, obj);
-    });
+      // каждому полю добавим обработчик события input
+      inputElement.addEventListener('input', () => {
+          // Внутри колбэка вызовем isValid,
+          // передав ей форму и проверяемый элемент
+          isValid(formElement, inputElement, params);
+          toggleButtonState(inputList, buttonElement, params);
+      });
   });
 };
 
-const enableValidation = (obj) => {
+const enableValidation = (params) => {
   // Найдём все формы с указанным классом в DOM,
   // сделаем из них массив методом Array.from
-  const formList = Array.from(document.querySelectorAll(obj.formSelector));
+  const formList = Array.from(document.querySelectorAll(params.formSelector));
   // Переберём полученную коллекцию
   formList.forEach((formElement) => {
-    formElement.addEventListener('submit', (evt) => {
-      // У каждой формы отменим стандартное поведение
-      evt.preventDefault();
-    });
+      formElement.addEventListener('submit', (evt) => {
+          // У каждой формы отменим стандартное поведение
+          evt.preventDefault();
+      });
 
-    // Для каждой формы вызовем функцию setEventListeners,
-    // передав ей элемент формы
-    setEventListeners(formElement, obj);
+      // Для каждой формы вызовем функцию setEventListeners,
+      // передав ей элемент формы
+      setEventListeners(formElement, params);
   });
 };
 
 // Вызовем функцию
 enableValidation(object);
-
