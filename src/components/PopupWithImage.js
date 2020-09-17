bc6188a1fe9e1c4134ca154f68c37f114c4cdd04
Yelena Strizhakova <e.strizhakova@gmail.com>
2020-09-17 10:13:16 +0300

    fix: overlay styles

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -4,7 +4,6 @@ import { openImage, pictureText } from '../utils/constants.js';
 export default class PopupWithImage extends Popup {
     constructor(popupSelector) {
       super(popupSelector);
-      this._color = 'rgba(0, 0, 0, .9)'; 
       this._overlay = this._popup.querySelector('.modal__overlay');
     }
   
@@ -15,7 +14,6 @@ export default class PopupWithImage extends Popup {
       openImage.src = cardData.link;
       openImage.alt = cardData.name;
       pictureText.textContent = cardData.name;
-      this._overlay.style.background = this._color;
     }
   
     close() {
